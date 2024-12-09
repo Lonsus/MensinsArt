b09d1d903906baf65d9ac6838a26af634077b0b6
lonsusklay <deobeso97@outlook.com>
2024-12-09 19:33:29 +0100

    actualización blog y script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,14 +20,53 @@ function showSection(target) {
                 return v.toString(16);
             });
         }
+         // Check admin status on page load
+         function checkAdminStatus() {
+            const isAdmin = localStorage.getItem('isAdmin') === 'true';
+            const adminForm = document.getElementById('admin-form');
+            const loginButton = document.getElementById('login-btn');
+            const logoutButton = document.getElementById('logout-btn');
+            if (isAdmin) {
+                adminForm.classList.remove('hidden');
+                loginButton.classList.add('hidden');
+                logoutButton.classList.remove('hidden');
+            } else {
+                adminForm.classList.add('hidden');
+                loginButton.classList.remove('hidden');
+                logoutButton.classList.add('hidden');
+            }
+        }
 
+        // Admin login function
+        function adminLogin() {
+            const username = prompt('Ingrese el nombre de usuario:');
+            const password = prompt('Por favor, ingrese la contraseña de administrador:');
+            const storedUsername = 'admin'; // Replace with username stored in config or database
+            const storedPassword = '123'; // Replace with password stored in config or database
+
+            if (username === storedUsername && password === storedPassword) {
+                localStorage.setItem('isAdmin', 'true');
+                alert('Bienvenido, Admin');
+                checkAdminStatus();
+            } else {
+                alert('Nombre de usuario o contraseña incorrectos');
+            }
+        }
+// Admin logout function
+function adminLogout() {
+    localStorage.removeItem('isAdmin');
+    alert('Sesión cerrada');
+    checkAdminStatus();
+}
         // Load saved posts on page load
-        document.addEventListener("DOMContentLoaded", loadPosts);
-
+        document.addEventListener('DOMContentLoaded', () => {
+            checkAdminStatus();
+            loadPosts();
+        });
         // Function to save posts to localStorage
         function savePostsToLocalStorage() {
             const posts = Array.from(document.querySelectorAll('.blog-post')).map(post => {
-                const id = post.dataset.id; // Primary Key
+                const id = post.dataset.id;
                 const img = post.querySelector('img')?.src || '';
                 const title = post.querySelector('h3').textContent;
                 const content = post.querySelector('p.blog-post-content').textContent;
@@ -35,42 +74,39 @@ function showSection(target) {
             });
             localStorage.setItem('blogPosts', JSON.stringify(posts));
         }
+       // Function to load posts from localStorage
+       function loadPosts() {
+        const posts = JSON.parse(localStorage.getItem('blogPosts') || '[]');
+        const postsContainer = document.getElementById('blog-posts');
+        posts.forEach(postData => {
+            const post = document.createElement('div');
+            post.classList.add('blog-post');
+            post.dataset.id = postData.id;
+            post.innerHTML = `
+                ${postData.img ? `<img src="${postData.img}" alt="Blog Image">` : ''}
+                <h3>${postData.title}</h3>
+                <p class="blog-post-content">${postData.content}</p>
+                <button class="delete-btn hidden" onclick="deletePost(this)">Eliminar</button>
+            `;
+            if (localStorage.getItem('isAdmin') === 'true') {
+                post.querySelector('.delete-btn').classList.remove('hidden');
+            }
+            postsContainer.appendChild(post);
+        });
+    }
 
-        // Function to load posts from localStorage
-        function loadPosts() {
-            const posts = JSON.parse(localStorage.getItem('blogPosts') || '[]');
-            const postsContainer = document.getElementById('blog-posts');
-            const existingIds = new Set();
-
-            posts.forEach(postData => {
-                if (!existingIds.has(postData.id)) {
-                    const post = document.createElement('div');
-                    post.classList.add('blog-post');
-                    post.dataset.id = postData.id;
-                    post.innerHTML = `
-                        ${postData.img ? `<img src="${postData.img}" alt="Blog Image">` : ''}
-                        <h3>${postData.title}</h3>
-                        <p class="blog-post-content">${postData.content}</p>
-                        <button class="delete-btn" onclick="deletePost(this)">Eliminar</button>
-                    `;
-                    postsContainer.appendChild(post);
-                    existingIds.add(postData.id);
-                }
-            });
-        }
 
         // Function to add a new post
         function addPost(event) {
             event.preventDefault();
-
-            const id = generateUUID(); // Assign a unique ID to the post
+            const id = Date.now().toString();
             const title = document.getElementById('title').value;
             const content = document.getElementById('content').value;
             const imageInput = document.getElementById('image');
             const postsContainer = document.getElementById('blog-posts');
             const post = document.createElement('div');
             post.classList.add('blog-post');
-            post.dataset.id = id; // Store the ID in a data attribute
+            post.dataset.id = id;
 
             if (imageInput.files && imageInput.files[0]) {
                 const reader = new FileReader();
@@ -79,8 +115,11 @@ function showSection(target) {
                         <img src="${e.target.result}" alt="Blog Image">
                         <h3>${title}</h3>
                         <p class="blog-post-content">${content}</p>
-                        <button class="delete-btn" onclick="deletePost(this)">Eliminar</button>
+                        <button class="delete-btn hidden" onclick="deletePost(this)">Eliminar</button>
                     `;
+                    if (localStorage.getItem('isAdmin') === 'true') {
+                        post.querySelector('.delete-btn').classList.remove('hidden');
+                    }
                     postsContainer.appendChild(post);
                     savePostsToLocalStorage();
                 };
@@ -89,8 +128,11 @@ function showSection(target) {
                 post.innerHTML = `
                     <h3>${title}</h3>
                     <p class="blog-post-content">${content}</p>
-                    <button class="delete-btn" onclick="deletePost(this)">Eliminar</button>
+                    <button class="delete-btn hidden" onclick="deletePost(this)">Eliminar</button>
                 `;
+                if (localStorage.getItem('isAdmin') === 'true') {
+                    post.querySelector('.delete-btn').classList.remove('hidden');
+                }
                 postsContainer.appendChild(post);
                 savePostsToLocalStorage();
             }
@@ -107,7 +149,6 @@ function showSection(target) {
             post.remove();
             savePostsToLocalStorage();
         }
- 
 
 // Load posts on page load
 document.addEventListener('DOMContentLoaded', () => {
